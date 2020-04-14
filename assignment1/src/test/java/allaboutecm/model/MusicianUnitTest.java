56cefdf14067dee007a4b24f9798830508b3aa0e
Ben <bjcla9@student.monash.edu>
2020-04-14 12:16:54 +1000

    initcommit for MusicianUnitTest - actually added this time?

---
+++ assignment1/src/test/java/allaboutecm/model/MusicianUnitTest.java
@@ -0,0 +1,16 @@
+package allaboutecm.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+public class MusicianUnitTest {
+    private Musician musician;
+
+}
