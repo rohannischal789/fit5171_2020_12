e6fd7cf74178fedf4f783f17f8093d2d353e0909
Ben <bjcla9@student.monash.edu>
2020-04-14 12:13:56 +1000

    initcommit for MusicianUnitTest

--- assignment1/src/main/java/allaboutecm/model/Musician.java
+++ assignment1/src/main/java/allaboutecm/model/Musician.java
@@ -45,7 +45,7 @@ public class Musician extends Entity {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Musician that = (Musician) o;
-        return Objects.equals(name, that.name);
+        return Objects.equals(name, that.name); //Delete later?
     }
 
     @Override
