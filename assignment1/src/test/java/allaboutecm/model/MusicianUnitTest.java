56503a314211b1d194bac2b045d4a9f01cc920ea
Rohan <rnis0005@student.monash.edu>
2020-04-17 14:12:50 +1000

    Minor bug in MusicianUnitTest fixed
    Removed unnecessary import in MusicianUnitTest

--- assignment1/src/test/java/allaboutecm/model/MusicianUnitTest.java
+++ assignment1/src/test/java/allaboutecm/model/MusicianUnitTest.java
@@ -1,6 +1,5 @@
 package allaboutecm.model;
 
-import jdk.nashorn.internal.runtime.ECMAException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
