ebccd3e985a21881797f6db5f446fece39439aa2
Rohan <rnis0005@student.monash.edu>
2020-05-16 14:25:33 +1000

    Initial test added for integration testing
    Initial setup done and test added for integration testing

--- assignment1/src/test/java/allaboutecm/mining/ECMMinerIntegrationTest.java
+++ assignment1/src/test/java/allaboutecm/mining/ECMMinerIntegrationTest.java
@@ -1,10 +1,82 @@
 package allaboutecm.mining;
 
+import allaboutecm.dataaccess.DAO;
+import allaboutecm.dataaccess.neo4j.Neo4jDAO;
+import allaboutecm.model.Album;
+import allaboutecm.model.Musician;
+import com.google.common.collect.Sets;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.neo4j.ogm.config.Configuration;
+import org.neo4j.ogm.session.Session;
+import org.neo4j.ogm.session.SessionFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * TODO: perform integration testing of both ECMMiner and the DAO classes together.
  */
 class ECMMinerIntegrationTest {
+    private static final String TEST_DB = "target/test-data/test-db.neo4j";
+
+    private static DAO dao;
+    private static Session session;
+    private static SessionFactory sessionFactory;
+    private ECMMiner ecmMiner;
+
+    @BeforeEach
+    public void setUp() {
+
+        Configuration configuration = new Configuration.Builder().build();
+
+        // Disk-based embedded store
+        //Configuration configuration = new Configuration.Builder().uri(new File(TEST_DB).toURI().toString()).build();
+
+        // HTTP data store, need to install the Neo4j desktop app and create & run a database first.
+        //Configuration configuration = new Configuration.Builder().uri("http://neo4j:password@localhost:7474").build();
+
+        sessionFactory = new SessionFactory(configuration, Musician.class.getPackage().getName());
+        session = sessionFactory.openSession();
+
+        dao = new Neo4jDAO(session);
+        ecmMiner = new ECMMiner(dao);
+    }
+
+    @AfterEach
+    public void tearDownEach() {
+        session.purgeDatabase();
+    }
+
+    @AfterAll
+    public static void tearDown() throws IOException {
+        session.purgeDatabase();
+        session.clear();
+        sessionFactory.close();
+        File testDir = new File(TEST_DB);
+        if (testDir.exists()) {
+            //FileUtils.deleteDirectory(testDir.toPath());
+        }
+    }
+
+    @Test
+    public void shouldReturnTheMusicianWhenThereIsOnlyOneProlific() {
+        Album album = new Album(1975, "ECM 1064/65", "The Köln Concert");
+        Musician musician = new Musician("Keith Jarrett");
+        musician.setAlbums(Sets.newHashSet(album));
+        dao.createOrUpdate(musician);
+
+        List<Musician> musicians = ecmMiner.mostProlificMusicians(5, -1, -1);
+
+        assertEquals(1, musicians.size());
+        assertTrue(musicians.contains(musician));
+    }
 
 }
